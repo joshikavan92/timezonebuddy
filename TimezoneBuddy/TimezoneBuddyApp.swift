ce8b56ed3b53ff4e148f3aaf91d9a194770f8757
joshikavan92 <joshikavan92@gmail.com>
2025-04-03 12:47:33 +0530

    Initial Commit

---
+++ TimezoneBuddy/TimezoneBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimezoneBuddyApp.swift
+//  TimezoneBuddy
+//
+//  Created by Kavan Joshi on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TimezoneBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
